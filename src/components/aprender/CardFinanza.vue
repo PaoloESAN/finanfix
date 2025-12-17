57339b5a27c0e63546c4d8979b82d63efe90bb4d
Paolo <186249117+PaoloESAN@users.noreply.github.com>
2025-12-17 15:03:55 -0500

    feat: seccion de aprender agregada

---
+++ src/components/aprender/CardFinanza.vue
@@ -0,0 +1,68 @@
+<script setup lang="ts">
+    import Card from 'primevue/card';
+    import Button from 'primevue/button';
+
+    const props = defineProps({
+        titulo: {
+            type: String,
+            required: true
+        },
+        subtitulo: {
+            type: String,
+            required: true
+        },
+        contenido: {
+            type: String,
+            required: true
+        },
+        imagen: {
+            type: String,
+            required: true
+        },
+        url: {
+            type: String,
+            required: true
+        }
+    })
+
+    const openUrl = (url: string) => {
+        window.open(url, '_blank', 'noopener, noreferrer')
+    }
+</script>
+
+<template>
+    <Card style="width: 25rem; overflow: hidden">
+        <template #header>
+            <img alt="header" :src="props.imagen" class="w-full h-52 object-cover" />
+        </template>
+        <template #title>{{ props.titulo }}</template>
+        <template #subtitle>{{ props.subtitulo }}</template>
+        <template #content>
+            <p class="m-0">
+                {{ props.contenido }}
+            </p>
+        </template>
+        <template #footer>
+            <div class="flex mt-1">
+                <Button label="Comenzar" class="w-full" @click="openUrl(props.url)" />
+            </div>
+        </template>
+    </Card>
+</template>
+<style scoped>
+:deep(.p-card) {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+}
+
+:deep(.p-card-body) {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+}
+
+:deep(.p-card-footer) {
+    margin-top: auto;
+}
+</style>
--- src/pages/dashboard/DashboardAprender.vue
+++ src/pages/dashboard/DashboardAprender.vue
@@ -1,9 +1,44 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+import CardFinanza from '../../components/aprender/CardFinanza.vue';
+const cards = ref([
+    {
+        titulo: 'Aprende a invertir aquí',
+        subtitulo: 'Aprende a invertir en criptomonedas, bolsa de valores y mucho más en este gran curso sobre inversión.',
+        contenido: '',
+        imagen: 'https://finanfix.wordpress.com/wp-content/uploads/2024/07/image.png?w=1024',
+        url: 'https://www.aprendeainvertir.co/'
+    },
+    {
+        titulo: 'Aprende a ahorrar aquí',
+        subtitulo: '¿No sabes como ahorrar? Este curso es perfecto para ti, en este curso aprenderás los mejores métodos para ahorrar que nadie dice.',
+        contenido: '',
+        imagen: 'https://finanfix.wordpress.com/wp-content/uploads/2024/07/image-1.png',
+        url: 'https://invertirjoven.com/guia-de-ahorro/'
+    },   
+    {
+        titulo: 'Aprende sobre tarjetas de crédito aquí',
+        subtitulo: '¿No sabes que tarjeta de crédito sacar o que ventajas tiene cada una? No te preocupes más, en FinanFix te lo solucionamos con este gran curso.',
+        contenido: '',
+        imagen: 'https://finanfix.wordpress.com/wp-content/uploads/2024/07/image-2.png?w=1024',
+        url: 'https://blog.kubofinanciero.com/tecnicas-para-sacar-provecho-a-tu-tarjeta-de-credito'
+    },
+    {
+        titulo: 'Aprende sobre criptomonedas aquí',
+        subtitulo: '¿No sabes que son las criptomonedas? ¿Quieres comprar bitcoins y esas cosas pero no sabes como? Tranquilo, aquí te lo explicaremos todo.',
+        contenido: '',
+        imagen: 'https://www.santander.com/content/dam/santander-com/es/stories/contenido-stories/2021/educacionfinanciera/im-storie-guia-para-saber-que-son-las-criptomonedas-3.jpg',
+        url: 'https://www.bancosantander.es/glosario/criptomonedas'
+    }
+])
 </script>
 
 <template>
     <div class="p-6">
-        <h1 class="text-2xl font-bold">Aprender</h1>
-        <p class="mt-4 text-surface-500">Bienvenido al dashboard de aprendizaje</p>
+        <p class="mt-4 text-surface-500">Aprender</p>
+        <h1 class="text-2xl font-bold">Busca información sobre finanzas</h1>
+        <div class="flex flex-wrap gap-8 justify-baseline mt-8">
+            <CardFinanza v-for="card in cards" :key="card.titulo" :titulo="card.titulo" :subtitulo="card.subtitulo" :contenido="card.contenido" :imagen="card.imagen" :url="card.url"/>
+        </div>
     </div>
 </template>
